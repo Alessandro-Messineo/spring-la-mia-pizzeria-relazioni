724ca675eafa0fc1b946ef9168ba74e537eaf789
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-02-21 18:41:31 +0100

    aggiunte crud per editare una offerta

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/SpecialOfferController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/SpecialOfferController.java
@@ -16,20 +16,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-
 @Controller
 @RequestMapping("/special-offer")
 public class SpecialOfferController {
-    
+
     @Autowired
     private SpecialOfferRepository specialOfferRepository;
 
-    @Autowired PizzaRepository pizzaRepository;
+    @Autowired
+    PizzaRepository pizzaRepository;
 
     @GetMapping("/create/{id}")
     public String create(@PathVariable Integer id, Model model) {
-        
+
         SpecialOffer specialOffer = new SpecialOffer();
 
         specialOffer.setPizza(pizzaRepository.findById(id).get());
@@ -51,4 +50,29 @@ public class SpecialOfferController {
         specialOfferRepository.save(formOffer);
         return "redirect:/pizza/" + idPizza;
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+
+        model.addAttribute("specialOffer", specialOfferRepository.findById(id).get());
+        model.addAttribute("edit", true);
+
+        return "special-offers/form-special-offer";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("specialOffer") SpecialOffer formOffer, BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "special-offers/form-special-offer";
+        }
+
+        Integer idPizza = formOffer.getPizza().getId();
+
+        specialOfferRepository.save(formOffer);
+
+        return "redirect:/pizza/" + idPizza;
+    }
+
 }

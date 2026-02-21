525460903f9081e67a0be9a4c215fc2f018280c5
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-02-21 13:21:01 +0100

    create crud per la creazione di una nuova offerta

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/SpecialOfferController.java
@@ -0,0 +1,54 @@
+package org.lessons.java.spring_la_mia_pizzeria_relazioni.controller;
+
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.model.SpecialOffer;
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.repository.PizzaRepository;
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.repository.SpecialOfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+@Controller
+@RequestMapping("/special-offer")
+public class SpecialOfferController {
+    
+    @Autowired
+    private SpecialOfferRepository specialOfferRepository;
+
+    @Autowired PizzaRepository pizzaRepository;
+
+    @GetMapping("/create/{id}")
+    public String create(@PathVariable Integer id, Model model) {
+        
+        SpecialOffer specialOffer = new SpecialOffer();
+
+        specialOffer.setPizza(pizzaRepository.findById(id).get());
+
+        model.addAttribute("specialOffer", specialOffer);
+        return "special-offers/form-special-offer";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("specialOffer") SpecialOffer formOffer,
+            BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "special-offers/form-special-offer";
+        }
+
+        Integer idPizza = formOffer.getPizza().getId();
+
+        specialOfferRepository.save(formOffer);
+        return "redirect:/pizza/" + idPizza;
+    }
+}

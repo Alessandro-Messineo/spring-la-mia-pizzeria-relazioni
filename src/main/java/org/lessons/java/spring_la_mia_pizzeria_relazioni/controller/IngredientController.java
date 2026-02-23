a42717d79152b8a39035d9847855da654effa8f1
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-02-23 13:02:37 +0100

    aggiunto repository e controller per ingredienti e fatto metodo index con relativa pagina

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/IngredientController.java
@@ -0,0 +1,26 @@
+package org.lessons.java.spring_la_mia_pizzeria_relazioni.controller;
+
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@RequestMapping("/ingredient")
+public class IngredientController {
+    
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @GetMapping
+    public String index(Model model) {
+
+        model.addAttribute("ingredients", ingredientRepository.findAll());
+        return "ingredients/index";
+    }
+    
+
+}
